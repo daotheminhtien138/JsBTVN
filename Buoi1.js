34b164e832b44d5b70f3089f33730b552021ee3d
daotheminhtien138 <dtmt138@gmail.com>
2024-08-28 01:57:08 +0700

    init project

---
+++ Buoi1.js
@@ -0,0 +1,9 @@
+function changeH1Text(){
+    document.getElementsByTagName('h1')[0].innerHTML = "Text mới"
+}
+function addNewTag(){
+    document.write("nội dung mới")
+}
+function printfConsole(){
+    console.log(5+6);
+}
